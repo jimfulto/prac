73e3e54670ab8e852b89321d3d8d03be75c5a1f2
Jim Fulto <jimfulto233@gmail.com>
2022-01-10 15:57:31 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'prac';
+  customerForm: FormGroup;
+
+  ngOnInit() {
+    this.customerForm = new FormGroup({
+      'customerName': new FormControl('', Validators.required),
+      'customerAge': new FormControl('', Validators.required),
+      'customerContact': new FormControl('', [Validators.required, Validators.pattern(/^\d{10}$/)]),
+      'customerEmail': new FormControl('', [Validators.required, Validators.email]),
+    });
+  }
+  onSubmit() {
+    console.log(this.customerForm.value);
+  }
 }
